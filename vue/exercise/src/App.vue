731e9837a89cda37e63ee11a3049b3675a35df07
ohdnf <jupyohong7@gmail.com>
2020-05-31 22:41:29 +0900

    Add vue exercise

---
+++ vue/exercise/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <button>Index</button>
+    <button>Lotto</button>
+    <button>Lunch</button>
+    <Index/>
+    <Lotto/>
+  </div>
+</template>
+
+<script>
+import Index from './components/Index'
+import Lotto from './components/Lotto'
+
+export default {
+  name: 'App',
+  components: {
+    Index,
+    Lotto,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
