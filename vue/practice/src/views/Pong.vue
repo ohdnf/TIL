604b7a9a438a7429d70e25c1670a39371880048f
ohdnf <jupyohong7@gmail.com>
2020-06-09 23:49:39 +0900

    Update Vue.js TIL: Vue-router

---
+++ vue/practice/src/views/Pong.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pong">
+    <h1>Pong</h1>
+    <p>{{ messageFromPing }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pong',
+  data() {
+    return {
+      messageFromPing: this.$route.query.message
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
