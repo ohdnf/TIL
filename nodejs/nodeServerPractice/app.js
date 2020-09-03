42912b141d3ade786a09db6b29e517797a428fa1
Jupyo Hong <jupyohong7@gmail.com>
2020-09-04 00:49:26 +0900

    섹션 2-5 완료 Request, Response 처리 DB 연동 Router 개선(모듈화)

--- nodejs/nodeServerPractice/app.js
+++ nodejs/nodeServerPractice/app.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const app = express()
+
+const passport = require('passport')
+// const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+const flash = require('connect-flash')
+
 const router = require('./router/index')
 
 app.listen(3000, function() {
@@ -12,6 +18,17 @@ app.use(express.static('public'))
 // Express.js의 Built-in body-parser 사용
 app.use(express.json())
 app.use(express.urlencoded( {extended : true } ));
+
+// passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 // Router 모듈화
 app.use(router)
--- nodejs/nodeServerPractice/router/join.js
+++ nodejs/nodeServerPractice/router/join.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const path = require('path')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // DB Setting
 const mysql = require("mysql")
+const passport = require('passport')
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -20,28 +25,32 @@ router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../public/join.html'))
 })
 
-router.post('/', (req, res) => {
-  const body = req.body
-  const name = body.nickname
-  const email = body.email
-  const password = body.password
+passport.use('local-join', new localStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done) {
+  const query = connection.query('select * from user where email=?', )
+}))
 
-  const sql = {email: email, name: name, password: password}
-  const query = connection.query('insert into user set ?', sql, (err, rows) => {
-    if (err) { throw err }
-    // console.log("New user joined!", rows)
-    // rows == OkPacket {
-    //   fieldCount: 0,
-    //   affectedRows: 1,
-    //   insertId: 5,
-    //   serverStatus: 2,
-    //   warningCount: 0,
-    //   message: '',
-    //   protocol41: true,
-    //   changedRows: 0
-    // }
-    else res.render('main.ejs', {id: rows.insertId, name: name})
+router.post('/', passport.authenticate('local-join', {
+    successRedirect: '/main',
+    failureRedirect: '/join',
+    failureFlash: true 
   })
-})
+)
+
+// router.post('/', (req, res) => {
+//   const body = req.body
+//   const name = body.nickname
+//   const email = body.email
+//   const password = body.password
+
+//   const sql = {email: email, name: name, password: password}
+//   const query = connection.query('insert into user set ?', sql, (err, rows) => {
+//     if (err) { throw err }
+//     else res.render('main.ejs', {id: rows.insertId, name: name})
+//   })
+// })
 
 module.exports = router;  // 다른 파일에서 main.js를 사용할 수 있게 exports 처리
